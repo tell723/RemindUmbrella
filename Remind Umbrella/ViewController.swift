173dd742635692ff9ce9ab632fa95ce3af917ab3
tell723 <terumu723@google.com>
2020-02-28 11:23:48 +0900

    Initial Commit

---
+++ Remind Umbrella/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remind Umbrella
+//
+//  Created by 渡邊輝夢 on 2020/02/28.
+//  Copyright © 2020 Terumu Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
