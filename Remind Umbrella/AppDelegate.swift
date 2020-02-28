689d7699be24fecf157b14dfff4f7b6586be49a3
tell723 <terumu723@google.com>
2020-02-28 16:04:21 +0900

    first commit

--- Remind Umbrella/AppDelegate.swift
+++ Remind Umbrella/AppDelegate.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+         UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]){ (granted, error) in
+            if granted {
+//                UNUserNotificationCenter.current().delegate = self
+                print("許可する")
+            } else {
+                print("許可しない")
+            }
+        }
+        
+        
         return true
     }
 
@@ -30,6 +42,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
     }
 
 
--- Remind Umbrella/ViewController.swift
+++ Remind Umbrella/ViewController.swift
@@ -7,14 +7,174 @@
 //
 
 import UIKit
+import UserNotifications
+import SwiftyJSON
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate, CLLocationManagerDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var hourPV: UIPickerView!
+    @IBOutlet weak var minPV: UIPickerView!
+    
+    var locationManager: CLLocationManager!
+    var latitude: CLLocationDegrees!
+    var longitude: CLLocationDegrees!
+    var location = ""
+    var weather = "" {
+        willSet{
+            switch newValue {
+            case "":
+                message = "天気情報が所得されていません"
+            case "Rain":
+                message = "傘忘れんなよ！！"
+            default:
+                message =  "傘はいりません"
+            }
+        }
+    }
+    var message = ""
+    let baseUrl = "https://api.openweathermap.org/data/2.5/weather?"
+    let apiKey = "06c2c12ef09f140ac6e2270864976fc4"
+    
+    var hour: Int!
+    var min: Int!
+    let hourList = [Int](0...23)
+    let minList = [Int](0...59)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupLocationManager()
+        
+        hourPV.delegate = self
+        hourPV.dataSource = self
+        minPV.delegate = self
+        minPV.dataSource = self
+        
+        self.locationLabel.text = "Location:"
     }
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 0 {
+            return hourList.count
+        } else if pickerView.tag == 1 {
+            return minList.count
+        } else {
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        if pickerView.tag == 0 {
+            return String(hourList[row])
+        } else if pickerView.tag == 1 {
+            return String(minList[row])
+        } else {
+            return ""
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    didSelectRow row: Int,
+                    inComponent component: Int) {
+        if pickerView.tag == 0 {
+            self.hour = hourList[row]
+        } else if pickerView.tag == 1 {
+            self.min = minList[row]
+        }
+        
+        setNotification()
+    }
+    
+    func setupLocationManager() {
+        locationManager = CLLocationManager()
+        guard let locationManager = locationManager else { return }
+        locationManager.requestAlwaysAuthorization()
+        
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedAlways {
+            locationManager.delegate = self
+            locationManager.distanceFilter = 100.0
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.first
+        self.latitude = location?.coordinate.latitude
+        self.longitude = location?.coordinate.longitude
+        
+        guard let lat = latitude, let lon = longitude else { return }
+        print("lat: \(lat) lon: \(lon)")
+        
+        let dispatchGroup = DispatchGroup()
+        let jsonString = "\(self.baseUrl)lat=\(lat)&lon=\(lon)&appid=\(self.apiKey)"
+        guard let url = URL(string: jsonString) else { return }
+        
+        dispatchGroup.enter()
+        let task: URLSessionTask =
+            URLSession.shared.dataTask(with: url,
+                                       completionHandler: {data, response, error in
+                                        guard let data = data else { return }
+                                        do {
+                                            let json = try? JSON(data: data)
+                                            self.weather = json!["weather"][0]["main"].stringValue
+                                            self.location = json!["name"].stringValue
+                                            print("weather: \(self.weather)")
+                                            print("name: \(self.location)")
+                                            dispatchGroup.leave()
+                                        } catch {
+                                            print(error)
+                                        }
+            })
+        task.resume()
+        dispatchGroup.notify(queue: .main) {
+            self.setNotification()
+        }
+    }
+    
+    func setNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = self.message
+        content.body = self.location
+        content.sound = UNNotificationSound.default
+        
+        var notificationTime = DateComponents()
+        notificationTime.hour = self.hour
+        notificationTime.minute = self.min
+        
+        let tirgger: UNNotificationTrigger
+        tirgger = UNCalendarNotificationTrigger(dateMatching: notificationTime,
+                                                repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "Timer",
+                                            content: content,
+                                            trigger: tirgger)
+        
+        UNUserNotificationCenter.current().add(request,
+                                               withCompletionHandler: nil)
+        
+        if let lat = self.latitude, let lon = self.longitude {
+            print("lat: \(lat) lon: \(lon)")
+        }
+        if location == "" {
+            self.locationLabel.text = "Location: 位置情報が取得できません"
+        } else {
+            self.locationLabel.text = "Location: \(location)"
+        }
+    }
+    
 
 }
 
